14e7128221b1e33d81bf2573805995c4dcfff2cc
Zaar <zaarinc@gmail.com>
2023-04-28 11:05:27 +0300

    program temp_1

--- Program.cs
+++ Program.cs
@@ -10,3 +10,49 @@
 {"1234", "1567", "-2", "computer science"} // -> {"-2"}
 {"Russia", "Denmark", "Kazan"} // -> {}
 */
+
+string[] initArray = GetArray();
+int count = WordsCount(initArray);
+string[] finalArray = ResultArray(initArray, count);
+Console.WriteLine($"[{String.Join(", ", initArray)}] -> [{String.Join(", ", finalArray)}]");
+
+string[] GetArray()
+{
+    Console.WriteLine("How many words would be in the array?");
+    int stringLength = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Please, enter the words: ");
+    string[] formedArray = new string[stringLength];
+    for (int i = 0; i < stringLength; i++)
+    {
+        formedArray[i] = Console.ReadLine()!;
+    }
+    return formedArray;
+}
+
+int WordsCount(string[] formedArray)
+{
+    int count = 0;
+    for (int i = 0; i < formedArray.Length; i++)
+    {
+        if (formedArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] ResultArray(string[] formedArray, int newLenght)
+{
+    string[] resultArray = new string[newLenght];
+    int k = 0;
+    for (int i = 0; i < formedArray.Length; i++)
+    {
+        if (formedArray[i].Length <= 3)
+        {
+            resultArray[k] = formedArray[i];
+            k++;
+        }
+    }
+    return resultArray;
+}
